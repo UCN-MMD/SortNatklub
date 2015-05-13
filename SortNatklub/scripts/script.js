0001f8e34e82692f725cc2e25bc22fc788e37e3b
Swollbos <dannbos@hotmail.com>
2015-05-13 18:27:14 +0200

    menu

--- SortNatklub/scripts/script.js
+++ SortNatklub/scripts/script.js
@@ -1,3 +1,21 @@
 ﻿$(document).on("click", ".burger", function () {
     $(this).toggleClass("selected");
+
+    if ($(".burger").hasClass("selected")) {
+        $("nav").css("left", "0");
+    }
+    else {
+        $("nav").css("left", "-285px");
+    }
+});
+
+$(window).scroll(function () {
+    var windowHeight = $(window).height();
+    var scrollHeight = $(window).scrollTop();
+    $("header").removeClass("boxShadow");
+
+    if (windowHeight < scrollHeight) {
+        $("header").addClass("boxShadow");
+        $("header").css("background-color", "#191919");
+    }
 });
