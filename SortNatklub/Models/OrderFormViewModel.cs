31c73ee86682376e144c2acf56b36bb7830e5d62
Swollbos <dannbos@hotmail.com>
2015-05-27 14:58:32 +0200

    lols

---
+++ SortNatklub/Models/OrderFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SortNatklub.Models
+{
+    public class OrderFormViewModel
+    {
+        public int OrderID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int Phone { get; set; }
+        public int Guests { get; set; }
+        public string Date { get; set; }
+        public string OrderMessage { get; set; }
+        public int Total { get; set; }
+    }
+}
